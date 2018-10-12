b94a9eff61fadbe71a7ca4267ae68ba28ef64c36
Eden Mendel <Eden@OSX-10.10-BASE>
2018-10-12 15:05:27 -0400

    new test function4

--- index.js
+++ index.js
@@ -5,28 +5,38 @@ var bodyParser = require('body-parser');
 var app = express();
 const PORT = process.env.PORT || 5000
 
-
+const courses = [
+	{ id : 1, name: 'course 1'},
+	{ id : 2, name: 'course 2'},
+	{ id : 3, name: 'course 3'},
+];
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
-	res.send('hello world');
+	res.send('hello world!!!');
 });
 
 app.get('/api/test', function (req, res) {
 	res.send([1,2,3]);
-})
-
-app.get('/apm/justin', function (req, res) {
-	res.send('im brother batman');
 });
 
 
-app.get('/apm/dwyer', function (req, res) {
-	res.send('hodor');
+app.get('/api/courses', function (req, res) {
+	res.send(courses);
 });
 
 
-app.get('/api/janja', function (req, res) {
-	res.send('<img src="https://umassathletics.com/images/2016/9/21/2016_17_um_TENNIS_0033_Janja_Kovacevic.jpg">');
+app.get('/api/courses/:id', function (req, res) {
+	var cc =courses.find(c=> c.id == parseInt(req.params.id));
+	if(!cc)
+		res.status(404).send('didnt find');
+
+	res.send(cc);
 });
+
+
+
+
+
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
