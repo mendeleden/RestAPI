4899fde474c896b9d4d1f0479cd46b82f6e284d3
Eden Mendel <Eden@OSX-10.10-BASE>
2018-10-11 21:11:14 -0400

    new test function2

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function (req, res) => {
+app.get('/', function (req, res) {
 	res.send('hello world');
 });
 
-app.get('/api/test', function (req, res) => {
+app.get('/api/test', function (req, res) {
 	res.send([1,2,3]);
 })
 app.listen(5000, () => console.log('listening onoo port 5000'));
