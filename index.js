a6212cce399032be6d0609f517791b7f91e4348e
Eden Mendel <Eden@OSX-10.10-BASE>
2018-10-11 20:56:58 -0400

    new test function

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 app.get('/api/test', (req, res) => {
 	res.send([1,2,3]);
 })
-app.listen(3000, () => console.log('listening onoo port 3000'));
+app.listen(5000, () => console.log('listening onoo port 3000'));
 
 
 
