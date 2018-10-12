a7e732360486fb5e29cfd0006bcccd6db3a31760
Eden Mendel <Eden@OSX-10.10-BASE>
2018-10-11 20:48:41 -0400

    real quick test

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+app.listen(3000, () => console.log('listening on port 3000'));
+
+
+
+
