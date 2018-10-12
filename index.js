1f8b4a4d2ebcec50dfe967b4777134f0fb515fa6
Eden Mendel <Eden@OSX-10.10-BASE>
2018-10-11 21:00:56 -0400

    new test function

--- index.js
+++ index.js
@@ -5,20 +5,13 @@ const PORT = process.env.PORT || 5000
 
 
 
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/', (req, res) => res.send('hello world'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  .get('/', (req, res) => {
-	res.send('hello world');
-	});
-
-  .get('/api/test', (req, res) => {
-	res.send([1,2,3]);
-	})
 
 
 
